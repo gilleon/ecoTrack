82b4b0df1fe41913eaf5ee1e39df8f692dd4e0b1
airlorm <elormnizi@gmail.com>
2025-07-30 13:26:59 +0300

    feat: add LogAction type for tracking environmental activities

---
+++ src/types/log.ts
@@ -0,0 +1,9 @@
+export type LogAction = 
+  | 'trash-pickup'
+  | 'recycling'
+  | 'wildlife-observation'
+  | 'trail-maintenance'
+  | 'invasive-species-removal'
+  | 'water-quality-testing'
+  | 'education-outreach'
+  | 'habitat-restoration';
