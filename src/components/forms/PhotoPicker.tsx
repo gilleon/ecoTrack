db29ec3a946721e836d69d706f5d1af88d46b22a
airlorm <elormnizi@gmail.com>
2025-07-31 20:56:30 +0300

    fix: update mediaTypes to use ImagePicker.MediaTypeOptions in PhotoPicker

--- src/components/forms/PhotoPicker.tsx
+++ src/components/forms/PhotoPicker.tsx
@@ -61,7 +61,7 @@ export const PhotoPicker: React.FC<PhotoPickerProps> = ({
     }
 
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: [ImagePicker.MediaType.Images],
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 0.8,
